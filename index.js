45827bddc434089972febe90f88a4f07eedba3d1
RHrakib1 <rakibhasanmd457@gmail.com>
2024-11-05 13:13:12 +0600

    get method done in the id find

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tkml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,13 +27,28 @@ async function run() {
     try {
         await client.connect();
 
+        app.get('/journey', async (req, res) => {
+            const coures = collectionOfJurney.find()
+            const result = await coures.toArray()
+            res.send(result)
+
+        })
+        app.get('/alltourists/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const tourist = await collectionOfJurney.findOne(query);
+            res.send(tourist);
+
+        });
+
+
 
         app.post('/journey', async (req, res) => {
             const user = req.body
-            console.log("this is user",user);
+            console.log("this is user", user);
             const result = await collectionOfJurney.insertOne(user)
             res.send(result)
-            console.log('this is result ',result);
+            console.log('this is result ', result);
         })
 
 
